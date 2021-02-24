2cf3fff1af4ed1ed4559e0eaf894b19549b792cb
CCaelumSstack <79490053+CCaelumSstack@users.noreply.github.com>
2021-02-23 19:21:07 -0500

    Update to use for loops
    Changing solution to implement for loops instead of repetitive print statements.

--- WeekOne/Assignments/AssignmentOne.java
+++ WeekOne/Assignments/AssignmentOne.java
@@ -15,32 +15,80 @@ public class AssignmentOne {
 	 */
 	public static void main(String[] args) {
 		System.out.println("1)");
-		System.out.println("*");
-		System.out.println("**");
-		System.out.println("***");
-		System.out.println("****");
-		System.out.println(".........");
+		for(int i = 0; i < 4; i++)
+		{
+			for(int j = 0; j < i + 1; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i = 0; i < 9; i++)
+		{
+			System.out.print(".");
+		}
 		System.out.println();
 		System.out.println("2)");
-		System.out.println("..........");
-		System.out.println("****");
-		System.out.println("***");
-		System.out.println("**");
-		System.out.println("*");
+		for(int i = 0; i < 10; i++)
+		{
+			System.out.print(".");
+		}
+		System.out.println();
+		for(int i = 4; i > 0; i--)
+		{
+			for(int j = 0; j < i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		System.out.println();
 		System.out.println("3)");
-		System.out.println("     *     ");
-		System.out.println("    ***    ");
-		System.out.println("   *****   ");
-		System.out.println("  *******  ");
-		System.out.println("...........");
+		for(int i = 0; i < 4; i++)
+		{
+			int numstars = 1 + (i * 2);
+			for(int j = 0; j < (11-numstars)/2; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j = 0; j < numstars; j++)
+			{
+				System.out.print("*");
+			}
+			for(int j = 0; j < (11-numstars)/2; j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		for(int i = 0; i < 11; i++)
+		{
+			System.out.print(".");
+		}
 		System.out.println();
 		System.out.println("4)");
-		System.out.println("............");
-		System.out.println("  *******");
-		System.out.println("   *****");
-		System.out.println("    ***");
-		System.out.println("     *");
+		for(int i = 0; i < 12; i++)
+		{
+			System.out.print(".");
+		}
+		System.out.println();
+		for(int i = 3; i >= 0; i--)
+		{
+			int numstars = 1 + (i * 2);
+			for(int j = 0; j < (11-numstars)/2; j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j = 0; j < numstars; j++)
+			{
+				System.out.print("*");
+			}
+			for(int j = 0; j < (11-numstars)/2; j++)
+			{
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
 	}
 
 }
